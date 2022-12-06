57a1645a626c8f3a5f5a034bceb769c3ddd3ecad
Jeevahasan Mahadevan <36784130+jeevahasan@users.noreply.github.com>
2022-12-05 21:30:43 -0500

    beautifications

---
+++ CleverKitchen/CleverKitchen/RecipeDetailsController.swift
@@ -0,0 +1,16 @@
+//
+//  RecipeDetailsController.swift
+//  CleverKitchen
+//
+//  Created by Mahadevan Jeevahasan on 12/5/22.
+//
+
+import UIKit
+
+class RecipeDetailsController: UIViewController {
+
+    override func viewDidLoad() {
+        super .viewDidLoad()
+    }
+    
+}
